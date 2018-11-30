d0dc1de0b7bbd5abd66d9a3d9ded404af8f7471b
mattsmith803 <mattsmith803@hotmail.com>
2018-11-30 14:49:39 -0500

    audio and video functions called in main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 import djvj.djvj_GUI as gui
 import djvj.pitch as audio_listener
+import djvj.Visual as video
 
 # initialize GUI
 gui.init()
@@ -10,6 +11,9 @@ gui.init()
 print("This will be passed to the audio listener: %s" % gui.audio_attr)
 
 print("This will be passed to the video instance: %s" % gui.video_attr)
+
+# initialize Visual
+VISUAL = video.Visual()
 
 # initialize audio listener
 # check if have .wav file argument
@@ -20,4 +24,9 @@ if len(sys.argv) > 1:
         gui.audio_attr, "wav", AUDIO_INPUT)
 else:
     # create live audio listener
-    AUDIO_LISTENER = audio_listener.AudioListener(gui.audio_attr, "live")
+    AUDIO_LISTENER = audio_listener.AudioListener(
+        gui.audio_attr, "live", VISUAL)
+
+
+AUDIO_LISTENER.pitch_detection.analyze_pitch(AUDIO_LISTENER.VISUAL)
+VISUAL.play_video()
