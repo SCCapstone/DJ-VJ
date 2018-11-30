2b9171354e7aa1f535f85aca4700f7332094c269
Lothrop Richards <lothropr@email.sc.edu>
2018-11-30 13:25:02 -0500

    Visual.py Pt 2

--- djvj/Visual.py
+++ djvj/Visual.py
@@ -3,36 +3,36 @@ import sys
 import os
 
 
-
-
-
 class Visual:
-	"""docstring for ClassName"""
+    """docstring for ClassName"""
     def __init__(self, Pitch):
-		
+        
         self.input = Pitch
 
-        while true:
-            if self.input < 150
-            	CAP.release()
-        		cv2.destroyAllWindows()
+        while True:
+            if self.input < 150 :
 
-            	MY_PATH = os.path.abspath(os.path.dirname(__file__))
-				PATH = os.path.join(MY_PATH, "../test/video1.mp4")
-				CAP = cv2.VideoCapture(PATH)
-				FRAME = CAP.read()
+                cv2.destroyAllWindows()
 
-				cv2.imshow('frame', FRAME)
+                MY_PATH = os.path.abspath(os.path.dirname(__file__))
+                PATH = os.path.join(MY_PATH, "../test/video1.mp4")
+                CAP = cv2.VideoCapture(PATH)
+                CAP.release()
 
-			elif self.input > 150
-				CAP.release()
-        		cv2.destroyAllWindows()
+                FRAME = CAP.read()
 
-				MY_PATH = os.path.abspath(os.path.dirname(__file__))
-				PATH = os.path.join(MY_PATH, "../test/video2.mp4")
-				CAP = cv2.VideoCapture(PATH)
-				FRAME = CAP.read()
+                cv2.imshow('frame', FRAME)
 
-				cv2.imshow('frame', FRAME)
+            elif self.input > 150 :
+                cv2.destroyAllWindows()
 
-		
+                MY_PATH = os.path.abspath(os.path.dirname(__file__))
+                PATH = os.path.join(MY_PATH, "../test/video2.mp4")
+                CAP = cv2.VideoCapture(PATH)
+                CAP.release()
+                
+                FRAME = CAP.read()
+
+                cv2.imshow('frame', FRAME)
+
+        
