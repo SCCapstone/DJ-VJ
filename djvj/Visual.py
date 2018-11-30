4bf2dc4dcbd76482b53b36519a4dd9956319148f
Lothrop Richards <lothropr@email.sc.edu>
2018-11-30 13:11:30 -0500

    Visual.py

---
+++ djvj/Visual.py
@@ -0,0 +1,38 @@
+import cv2
+import sys
+import os
+
+
+
+
+
+class Visual:
+	"""docstring for ClassName"""
+    def __init__(self, Pitch):
+		
+        self.input = Pitch
+
+        while true:
+            if self.input < 150
+            	CAP.release()
+        		cv2.destroyAllWindows()
+
+            	MY_PATH = os.path.abspath(os.path.dirname(__file__))
+				PATH = os.path.join(MY_PATH, "../test/video1.mp4")
+				CAP = cv2.VideoCapture(PATH)
+				FRAME = CAP.read()
+
+				cv2.imshow('frame', FRAME)
+
+			elif self.input > 150
+				CAP.release()
+        		cv2.destroyAllWindows()
+
+				MY_PATH = os.path.abspath(os.path.dirname(__file__))
+				PATH = os.path.join(MY_PATH, "../test/video2.mp4")
+				CAP = cv2.VideoCapture(PATH)
+				FRAME = CAP.read()
+
+				cv2.imshow('frame', FRAME)
+
+		
