40b0e5bfd17f76a5cb82e9fa00cdcb88c257157b
aholdeman <holdeman2011ala@gmail.com>
2019-01-29 15:36:11 -0500

    Testing so far, having trouble with import

---
+++ test/pickle_unittest.py
@@ -0,0 +1,17 @@
+"""
+Runs a simple behavioral test.
+As the user, I wish to be able to load the program,
+click on "Create Show",
+and have the Create Show screen load
+This is automated using the pyautogui python package
+"""
+
+import pyautogui
+import djvj.djvj_GUI as gui
+
+if __name__ == '__main__':
+    print("Start Test")
+    gui.IntroScreen()
+    print("Click on Create Show")
+    pyautogui.click(x=461, y=620, clicks=2, button='left')
+    pyautogui.alert('This worked')
