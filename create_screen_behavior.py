5c6ddd4610cdc3974200395fa65c6fc1667924c4
aholdeman <holdeman2011ala@gmail.com>
2019-01-31 21:44:37 -0500

    Moved behavioral test so it would work

--- create_screen_behavior.py
+++ create_screen_behavior.py
@@ -5,13 +5,15 @@ click on "Create Show",
 and have the Create Show screen load
 This is automated using the pyautogui python package
 """
-
-import pyautogui
 import djvj.djvj_GUI as gui
+import pyautogui
+import time, sys
 
 if __name__ == '__main__':
     print("Start Test")
     gui.IntroScreen()
     print("Click on Create Show")
-    pyautogui.click(x=461, y=620, clicks=2, button='left')
-    pyautogui.alert('This worked')
+    pyautogui.click(x=461, y=620, clicks=1, button='left')
+    pyautogui.alert("Show created!")
+
+
