bd3dec0958aa1abf973f2d2c96cf48035d97bdbf
Michael Dyar <michael.dyar18@gmail.com>
2019-02-28 04:11:43 -0500

    It worked but weirdly

--- djvj/testVisuals.py
+++ djvj/testVisuals.py
@@ -17,15 +17,19 @@ def do_test():
     
     show = Visual()
     #Play the first video
+    print("We here")
     my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, "../test/test_assets/video1.MOV")
+    path = os.path.join(my_path, "../test/test_assets/video2.mp4")
+    show.currShow = path
     #playVideo = threading.Thread(target=play_video, args=(show, path))
     #playVideo.start()
     play_video(show, path)
-    time.sleep(5) #5 second delay
+    time.sleep(3) #5 second delay
+    print("we back")
     #Send the same video again to see if original video keeps playing
-    play_video(show, path)
+    #play_video(show, path)
     time.sleep(3)
+    print("back again")
     #Send new video to see if it changes
-    path = os.path.join(my_path, "../test/test_assets/video2.mp4")
+    path = os.path.join(my_path, "../test/test_assets/video1.MOV")
     play_video(show, path)
--- djvj/visual.py
+++ djvj/visual.py
@@ -24,32 +24,52 @@ def play_video(self, video):
     """ plays the video """
     #my_path = os.path.abspath(os.path.dirname(__file__))
     #self.newShow = video
-    if self.currShow == None:
-        self.currShow = video
-    elif self.currShow == video:
-        pass
-    else:
-        self.currShow = video
+    #if self.currShow == None:
+        #self.currShow = video
+    #elif self.currShow == video:
+        #pass
+    #else:
+        #self.currShow = video
     #path = os.path.join(my_path, "../test/test_assets/video1.MOV")
-    cap = cv2.VideoCapture(self.currShow)    # open first video
-    # print("now here")
-    while cap.isOpened():
-        #if self.currShow != video:
-             #self.currShow = video
-             #cap.cv2.VideoCapture(self.currShow)
-        ret, frame = cap.read()     # play video
-        try:
-            (h, w) = frame.shape[:2]
-            center = (w / 2, h / 2)
-        except:
-            break
-        try:
-            M = cv2.getRotationMatrix2D(center, -90, 1.0)
-            frame = cv2.warpAffine(frame, M, (w, h))  # rotate video
-            frame = cv2.resize(frame, (self.window_y, self.window_x))
-            cv2.imshow('video', frame)  # play video
-        except:
-            cap.release()
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    daShow = self.currShow
+    if self.currShow == video:
+        cap = cv2.VideoCapture(self.currShow)    # open first video
+        # print("now here")
+        while cap.isOpened():
+            ret, frame = cap.read()     # play video
+            try:
+                (h, w) = frame.shape[:2]
+                center = (w / 2, h / 2)
+            except:
+                break
+            try:
+                M = cv2.getRotationMatrix2D(center, -90, 1.0)
+                frame = cv2.warpAffine(frame, M, (w, h))  # rotate video
+                frame = cv2.resize(frame, (self.window_y, self.window_x))
+                cv2.imshow('video', frame)  # play video
+            except:
+                cap.release()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    if self.currShow != video:
+        self.currShow = video
+        cap = cv2.VideoCapture(self.currShow)    # open first video
+        # print("now here")
+        while cap.isOpened():
+            ret, frame = cap.read()     # play video
+            try:
+                (h, w) = frame.shape[:2]
+                center = (w / 2, h / 2)
+            except:
+                break
+            try:
+                M = cv2.getRotationMatrix2D(center, -90, 1.0)
+                frame = cv2.warpAffine(frame, M, (w, h))  # rotate video
+                frame = cv2.resize(frame, (self.window_y, self.window_x))
+                cv2.imshow('video', frame)  # play video
+            except:
+                cap.release()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     
