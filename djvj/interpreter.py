1b3d22371eb0a6c82f85028bf89a8b7b5ee8e153
mattsmith803 <mattsmith803@hotmail.com>
2019-03-03 01:37:37 -0500

    created Moments class, updated djvj/ to work with Moments

--- djvj/interpreter.py
+++ djvj/interpreter.py
@@ -5,6 +5,7 @@ needs to be played based on the Show.curr_param_values
 """
 
 import operator
+import time
 from collections import defaultdict
 
 
@@ -14,15 +15,16 @@ class Interpreter:
 
     def __init__(self, show):
         self.show = show  # the show Interpreter is interpreting
+        self.sleep = .1
         self.rules = defaultdict(list)  # the set of rules to interpret by
 
         self.ops = {
-            "<": operator.lt,
+            "<": operator.le,
             ">": operator.gt,
             "=": operator.eq,
         }
 
-        self.create_rules_table()
+        # self.create_rules_table()
 
     def create_rules_table(self):
         """
@@ -33,6 +35,41 @@ class Interpreter:
         # get indices of video rules
         for index, vid in enumerate(self.show.videos):
             self.rules[vid].append(index)
+
+    def interpret(self):
+        """
+        moment based interpreter
+        """
+        sleep = 0
+        # main interpreter loop
+        while True:
+            if self.show.curr_video != "":
+                sleep = self.sleep
+            # possible moments
+            possible_moments = {}
+            # for each moment
+            for moment in self.show.moments:
+                # check if moment video is already the current video being played
+                if moment.video == self.show.curr_video:
+                    continue
+                # for each param in a moment
+                for index, param in enumerate(moment.params):
+                    # compare current audio param value to show rule value
+                    # using rule operator
+                    if self.ops[moment.operators[index]](self.show.curr_param_values[param], moment.values[index]):
+                        possible_moments[moment] = [moment.name, param]
+                        # time.sleep(sleep)
+                        continue
+                    # otherwise remove index (or do nothing)
+                    possible_moments.pop(moment, None)
+                    # time.sleep(sleep)
+                    break
+            if len(possible_moments) != 1:
+                time.sleep(sleep)
+                continue
+            moment, _ = possible_moments.popitem()
+            self.show.curr_video = moment.video
+            time.sleep(sleep)
 
     # def interpret(self):
     #     """
@@ -55,35 +92,36 @@ class Interpreter:
     #                     continue
     #                 # otherwise remove index (or do nothing)
     #                 possible_videos_index.pop(index, None)
+    #                 break
     #
-    #         if len(possible_videos_index) == 1:
-    #             _, self.show.curr_video = possible_videos_index.popitem()
-    #             print(self.show.curr_video)
-    #             continue
-    #         print("Show Logic Error")
-    #         print(possible_videos_index)
+            # if len(possible_videos_index) == 1:
+            #     _, self.show.curr_video = possible_videos_index.popitem()
+            #     print(self.show.curr_video)
+            #     continue
+            # print("Show Logic Error")
+            # print(possible_videos_index)
 
-    def interpret(self):
-        """
-        video based interpreter
-        """
-        while True:
-            found = False
-            # for each video in rules
-            for video in self.rules:
-                # check if video is already the current video being played
-                if video != self.show.curr_video:
-                    # for each rule for video
-                    for index in self.rules[video]:
-                        # get what param is being checked
-                        index_param = self.show.params[index]
-                        # compare current audio param value to show rule value
-                        # using rule operator
-                        if self.ops[self.show.operator[index]](self.show.curr_param_values[index_param], self.show.values[index]):
-                            found = True
-                            continue
-                        found = False
-                        break
-                    if found:
-                        self.show.curr_video = video
-                        break
+    # def interpret(self):
+    #     """
+    #     video based interpreter
+    #     """
+    #     while True:
+    #         found = False
+    #         # for each video in rules
+    #         for video in self.rules:
+    #             # check if video is already the current video being played
+    #             if video != self.show.curr_video:
+    #                 # for each rule for video
+    #                 for index in self.rules[video]:
+    #                     # get what param is being checked
+    #                     index_param = self.show.params[index]
+    #                     # compare current audio param value to show rule value
+    #                     # using rule operator
+    #                     if self.ops[self.show.operator[index]](self.show.curr_param_values[index_param], self.show.values[index]):
+    #                         found = True
+    #                         continue
+    #                     found = False
+    #                     break
+    #                 if found:
+    #                     self.show.curr_video = video
+    #                     break
