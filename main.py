c11548d4d9fed4d0c56a6430166bacf4694b04dc
mattsmith803 <mattsmith803@hotmail.com>
2019-03-03 17:43:09 -0500

    create moment class to handle GUI moments, updated main to reflect moments

--- main.py
+++ main.py
@@ -1,44 +1,11 @@
 """ main run file for the project"""
-import djvj.gui as gui
+# import djvj.gui as gui
+import djvj.gui_moments as gui
+import djvj.moment as moment
 import djvj.show as show
 
-
-class Moment:
-
-    def __init__(self, name, params, operators, values, video):
-        self.name = name
-        self.params = params
-        self.operators = operators
-        self.values = values
-        self.video = video
-
-
-# one param test
-# MOMENT_1 = Moment("moment1", ['time'], ['<'], [10],
-#                   "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV")
-# MOMENT_2 = Moment("moment2", ['time'], ['>'], [10],
-#                   "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4")
-# SHOW_PARAMS = [MOMENT_1, MOMENT_2]
-
-# two param test, doesnt make sense after 10 seconds
-# MOMENT_1 = Moment("moment1", ['time', 'pitch'], ['<', '<'], [10, 500],
-#                   "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV")
-# MOMENT_2 = Moment("moment2", ['time', 'pitch'], ['<', '>'], [10, 500],
-#                   "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4")
-# SHOW_PARAMS = [MOMENT_1, MOMENT_2]
-
-# pitch rules reverse after 10 seconds
-MOMENT_1 = Moment("moment1", ['time', 'pitch'], ['<', '<'], [15, 500],
-                  "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV")
-MOMENT_2 = Moment("moment2", ['time', 'pitch'], ['<', '>'], [15, 500],
-                  "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4")
-MOMENT_3 = Moment("moment3", ['time', 'pitch'], ['>', '<'], [15, 500],
-                  "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4")
-MOMENT_4 = Moment("moment4", ['time', 'pitch'], ['>', '>'], [15, 500],
-                  "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV")
-SHOW_PARAMS = [MOMENT_1, MOMENT_2, MOMENT_3, MOMENT_4]
-
 # initialize GUI
+<<<<<<< HEAD
 # gui.init()
 
 # once a show is loaded, audio_attr/video_attr is initialized
@@ -55,61 +22,24 @@ print(gui.moments)
 
 # SHOW_PARAMS = [['pitch', 'pitch'],
 #                ['<', '>'], [500, 500], ["/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV", "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4"]]
+=======
+gui.init()
+# print(gui.moments)
+MOMENTS = moment.create_moments(gui.moments)
+>>>>>>> create moment class to handle GUI moments, updated main to reflect moments
 
-# SHOW_PARAMS = [['tempo', 'tempo'],
-#                ['<', '>'], [120, 120], ["/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV", "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4"]]
-
-# SHOW_PARAMS = [['time', 'time'],
-#                ['<', '>'], [10, 10], ["/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV", "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4"]]
-
-# SHOW_PARAMS = [['pitch', 'time', 'pitch', 'time'],
+# gui_moments_list = [[['pitch', '<', '500', '/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV'], ['time', '<', '10', '/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV']], [['pitch', '>', '500', '/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4'], ['time', '<', '10', '/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4']],
+#                     [['pitch', '<', '500', '/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4'], ['time', '>', '10', '/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4']], [['pitch', '>', '500', '/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV'], ['time', '>', '10', '/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV']], []]
 #
-#                ['<', '<', '>', '<'],
-#
-#                [500, 10, 500, 10],
-#
-#                ["/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4"]]
-"""
-need instance of videos???? to account for multiple of the same parameter for one video
-
-or make interpreter param based instead of video
-"""
-"""
-During first 30 seconds, pitch < 500 video1, pitch > 500 video2
-During second 30 seconds, pitch < 500 video1, pitch > 500 video1
-
-0: if pitch < 500 video1
-1: if time < 30 video1
-2: if pitch > 500 video2
-3: if time < 30 video2
-
-4: if pitch < 500 video2
-5: if time > 30 video2
-6: if pitch > 500 video1
-7: if time > 30 video1
-"""
-
-# SHOW_PARAMS = [['pitch', 'time', 'pitch', 'time', 'pitch', 'time', 'pitch', 'time'],
-#
-#                ['<', '<', '>', '<', '<', '>', '>', '>'],
-#
-#                [500, 30, 500, 30, 500, 30, 500, 30],
-#
-#                ["/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video2.mp4",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV",
-#                 "/Users/matt/USC/CSCE_490/DJ-VJ/test/test_assets/video1.MOV"]]
+# MOMENTS = moment.create_moments(gui_moments_list)
 
 
 # initialize show
+<<<<<<< HEAD
 #SHOW = show.Show(SHOW_PARAMS)
+=======
+SHOW = show.Show(MOMENTS)
+>>>>>>> create moment class to handle GUI moments, updated main to reflect moments
 
 # start show
 #SHOW.start()
