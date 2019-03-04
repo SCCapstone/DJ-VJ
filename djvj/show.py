71c8cdf557920b42606a0a835c19f46f62fbdd40
mattsmith803 <mattsmith803@hotmail.com>
2019-03-03 19:40:26 -0500

    added possible moments = 0 case to interpreter

--- djvj/show.py
+++ djvj/show.py
@@ -73,8 +73,8 @@ class Show:
             # start video player
             # compares show.curr_video to video_player.curr_video and
             # updates accordingly
-            # print("Playing video")
-            # self.video_player.play_video()
+            print("Playing video")
+            self.video_player.play_video()
 
         except KeyboardInterrupt:
             pass
