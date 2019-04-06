28b4f8ec8e46c64d8b2427df8b4caa24f67e7591
Lothrop Richards <lothropr@email.sc.edu>
2019-04-05 22:35:33 -0400

    Indent update

--- djvj/Bugger.py
+++ djvj/Bugger.py
@@ -2,9 +2,9 @@
 
 class Bugger:
 
-	def __init__(self, temp_momment, official_momment):
+    def __init__(self, temp_momment, official_momment):
 		#current param
-		self.t_param = temp_momment[1]
+        self.t_param = temp_momment[1]
         #sign
         self.t_sign = temp_momment[2]
         #value
@@ -20,29 +20,29 @@ class Bugger:
         #main moment that will be used in the show class
         self.official_momment = official_momment
 
-	def momment_check(self):
+    def momment_check(self):
 		# if the main momment is empty
-		if official_momment != True:
-			return True
-		'''
+        if official_momment != True:
+            return True
+        '''
 		Iterates through the lists within the list
 		  List1.   List2    List2
 		[[A,B,C], [D,E,F], [G,H,I]]
 		'''
-		for x in self.official_momment:
-			'''
+        for x in self.official_momment:
+            '''
 			Iterates through the objects within a single list
 			 Object1 Object2 Object3
 			[A,      B,      C]
 			'''
-			for y in x:
-				'''
+            for y in x:
+                '''
 				compare current momment param to existing momment param
 				ex:
 				Pitch = Pitch 
 				''' 
-				if t_param == y[1]:
-					'''
+                if t_param == y[1]:
+                    '''
 					if current momment meets the condition of any existing momments, flag it
 					ex:
 					t_value = 3
@@ -51,16 +51,16 @@ class Bugger:
 					if tvalue < y[3]
 					flag this as a possible collision 
 					'''
-					if self.ops[y[2]](self.value, y[3]):
-						self.strike += 1
+                    if self.ops[y[2]](self.value, y[3]):
+                        self.strike += 1
 			
 			# if the value is found to be in between two existing condition (strike is equal 2), return false
-			if self.strike >= 2:
-				return False
+            if self.strike >= 2:
+                return False
 
-			self.strike = 0
+            self.strike = 0
 
-		return True
+        return True
 
 			#counter that iterates through existing moments 
 
