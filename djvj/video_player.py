7a8b5fcca0fc3c59b53124000207255321bcd9c1
mattsmith803 <mattsmith803@hotmail.com>
2019-04-07 22:51:16 -0400

    added frame queuing, need to figure out looping behavior

--- djvj/video_player.py
+++ djvj/video_player.py
@@ -5,8 +5,10 @@ __author__ = "Matthew J. Smith, Lothrop Richards, Timothy Dyar"
 __email__ = "mjs10@email.sc.edu, lothropr@email.sc.edu, tdyar@email.sc.edu"
 """
 import os
-import numpy as np
 import tkinter
+import threading
+import queue
+import numpy as np
 import cv2 as visual
 
 
@@ -18,7 +20,10 @@ class VideoPlayer:
 
     def __init__(self, show):
         self.curr_video = ""  # keeps track of the current video to be played
+        self.switched = False  # keeps up with if video was switched
         self.show = show  # show instance that is using this video_player
+        self.queue = queue.Queue(maxsize=100)  # queue to hold loaded frames
+        self.cap = visual.VideoCapture()  # video capture object
 
         # get screen dimensions
         root = tkinter.Tk()
@@ -50,20 +55,26 @@ class VideoPlayer:
             # add video path to current path
             video = os.path.join(my_path, self.curr_video)
             # open video
-            cap = visual.VideoCapture(video)
+            self.cap = visual.VideoCapture(video)
+            # update switched
+            self.switched = False
+
+            # start queue thread
+            queue_thread = threading.Thread(target=queue_frames, args=(self,))
+            queue_thread.start()
 
             # main playback loop - plays the video
-            while cap.isOpened():
+            while self.cap.isOpened():
                 # check signals
                 # if pause
                 if self.pause:
                     # release current video
-                    cap.release()
+                    self.cap.release()
                     # closes playing window
                     # pause the video
                     self.pause_video()
                     # after unpausing, open video
-                    cap = visual.VideoCapture(video)
+                    self.cap = visual.VideoCapture(video)
 
                 # if kill
                 if self.kill:
@@ -74,14 +85,17 @@ class VideoPlayer:
 
                 # check if current video has been updated
                 if self.curr_video != self.show.curr_video:
-                    break  # if so, break to update player current video
+                    # update switched
+                    self.switched = True
+                    # break to update player current video
+                    break
 
-                # get next frame: returns bool, image
-                _, frame = cap.read()
+                # check of queue is empty
+                if self.queue.qsize() == 0:
+                    continue
 
-                # resize the frame
-                frame = visual.resize(
-                    frame, (self.window_y, self.window_x))
+                # get next frame from queue
+                frame = self.queue.get()
 
                 # show the frame
                 visual.imshow('window', frame)
@@ -125,3 +139,21 @@ class VideoPlayer:
                 self.kill = True
                 return
         return
+
+
+def queue_frames(video_player):
+    while not video_player.kill:
+        # check if video has been switched
+        if video_player.switched is True:
+            video_player.queue.empty()
+            return
+        # check if queue is full
+        if video_player.queue.full():
+            continue
+        # get frame
+        _, frame = video_player.cap.read()
+        # resize the frame
+        frame = visual.resize(
+            frame, (video_player.window_y, video_player.window_x))
+        # add to queue
+        video_player.queue.put(frame)
