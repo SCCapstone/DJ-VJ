c5b27fe048c947ee81175973bbcd5d8b3d85774c
Michael Dyar <michael.dyar18@gmail.com>
2019-04-29 23:49:43 -0400

    unittesting stuff

--- test/unittesting.py
+++ test/unittesting.py
@@ -1,19 +1,18 @@
 import unittest
 import threading
-import djvj.audio_listener as audio
-import djvj.interpreter as interpreter
-import djvj.video_player as video_player
-import djvj.show as show
+#import audio_listener as audio
+#import interpreter as interpreter
+import video_player as video_player
+import show as show
 
 class Testing(unittest.TestCase):
 
 	def test_video(self):
 		SHOW = show.SHOW("")
-		my_path = os.path.abspath(os.path.dirname(__file__))
 		SHOW.curr_video = os.path.join(my_path, "../test/test_assets/Eat_it_Duke.mp4")
 		SHOW.video_player.play_video()
 
-suite = unittest.TestLoader().loadTestsFromTestCase(Testing)
+suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 	
