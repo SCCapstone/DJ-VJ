bc53eda47f7d9a2c70e733f2b7e853cf93795483
Michael Dyar <michael.dyar18@gmail.com>
2019-04-30 03:01:50 -0400

    fixed module errors bugs

--- test/unittesting.py
+++ test/unittesting.py
@@ -1,18 +1,19 @@
 import unittest
 import threading
-#import audio_listener as audio
-#import interpreter as interpreter
-import video_player as video_player
-import show as show
+import djvj.audio_listener as audio
+import djvj.interpreter as interpreter
+import djvj.video_player as video_player
+import djvj.show as show
 
 class Testing(unittest.TestCase):
 
 	def test_video(self):
 		SHOW = show.SHOW("")
+		my_path = os.path.abspath(os.path.dirname(__file__))
 		SHOW.curr_video = os.path.join(my_path, "../test/test_assets/Eat_it_Duke.mp4")
 		SHOW.video_player.play_video()
 
-suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
+suite = unittest.TestLoader().loadTestsFromTestCase(Testing)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 	
